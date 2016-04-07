0a45e65fcede8872f1ce41862aadc69ea369149a
Don Henton <donhenton@gmail.com>
2016-04-07 15:46:02 -0500

    documentation

--- src/main/front-end-source/browserify-es6/mainFile.js
+++ src/main/front-end-source/browserify-es6/mainFile.js
@@ -1,9 +1,20 @@
 var polygonModule = require('./polygonModule');
 
-
+/**
+ * this file illustrates the interfacing of browserified code
+ * with code in the browser environment
+ * 
+ * Javascript in the browser doesn't support browserify so this
+ * function bridges out of the world of 'require' and into the
+ * normal browser world by assigning the browserify functionality
+ * to a global variable on the winow object.
+ * 
+ * @param {type} param
+ */
 
 $('document').ready(function () {
     window.polygonFactory = new polygonModule();
+    //evokePolygon is called by a onclick method in the html page
     window.evokePolygon = function ()
     {
         var item = polygonFactory.create(3, 'square');
